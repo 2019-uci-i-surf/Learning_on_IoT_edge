b966dfc0d0d7ec97ca6998da53c9e3f2d5f37681
Unsang <iloveunhs@naver.com>
2019-08-19 13:09:44 -0700

    test

---
+++ server.py
@@ -0,0 +1,41 @@
+from mobilenettest import MobileNetTest
+from socket import socket, AF_INET, SOCK_STREAM
+import numpy
+from threading import Thread
+from settings import *
+import time
+from clientinstance import ClientInstance
+
+class Server:
+    def __init__(self, host, port): # open socket
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind((host, port))
+        self.mobile_net_test = MobileNetTest(CLASS_NAMES, WEIGHT_PATH, INPUT_SHAPE)
+        self.ci_list = []
+
+    def network_thread(self): # connect client
+        self.socket.listen(1)
+        print('Ready to accept client')
+
+        thread_list = []
+        for _ in range(NUMBER_OF_SEND_VIDEO):
+            conn, addr = self.socket.accept()
+            print('successfully connected', addr[0], ':', addr[1])
+            ci = ClientInstance(self.mobile_net_test, conn)
+            conn_thread = Thread(target=ci.main_task)
+            conn_thread.start()
+            self.ci_list.append(ci)
+            thread_list.append(conn_thread)
+
+        for conn_thread in thread_list:
+            conn_thread.join()
+
+    def run_task(self):
+        # communication thread section
+        network_thread = Thread(target=self.network_thread)
+        network_thread.start()
+        network_thread.join()
+
+if __name__ == '__main__':
+    server = Server(SERVER_HOST, SERVER_PORT)
+    server.run_task()
--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
-SERVER_HOST = '10.8.38.127'
-#SERVER_PORT = 9999
+SERVER_HOST = 'localhost'
+SERVER_PORT = 10001
 
+INPUT_SHAPE = (640, 360, 3)
 WEIGHT_PATH = 'mobilenetssd300.hdf5'
 CLASS_NAMES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
                "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train",
                "tvmonitor"]
 DETECTION_LIST = ['person', 'chair', 'boat']
+NUMBER_OF_SEND_VIDEO = 2
 
 # This image queue size. Queue was gotten from the model
 TCP_QUEUE_SIZE = 10
